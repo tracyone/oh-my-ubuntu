614ffb6711d784cebc472414dd264a5b4989f3fb
tracyone <tracyone@live.cn>
2015-12-30 23:21:25 +0800

    首次提交

---
+++ software_install.sh
@@ -0,0 +1,235 @@
+#!/bin/bash
+# author:tracyone,tracyone@live.cn
+
+# 判断是32位还是64位系统
+# {{{
+function AptInstall()
+{
+	read -n1 -p "Install $1 ?(y/n)" ans
+	if [[ $ans =~ [Yy] ]]; then
+		sudo apt-get install $1 --allow-unauthenticated -y
+	else
+		echo "Do not install $1"
+	fi
+	sleep 2
+}
+# }}}
+
+MACHINE_TYPE=`uname -m`
+if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+  is_64=1
+else
+  is_64=0
+fi
+
+shopt -s expand_aliases
+read -p "请输入您的密码:" mypasswd
+alias sudo="echo "${mypasswd}" | sudo -S"
+
+if [[ -d ~/Work ]]; then
+	mkdir -p ~/Work
+fi
+cd ~/Work
+
+mkdir ./temp
+echo "添加仓库------------------------------------"
+echo "添加 git 仓库..."
+sudo add-apt-repository -y ppa:git-core/ppa
+echo "添加 pidgin 仓库..."
+sudo add-apt-repository -y ppa:lainme/pidgin-lwqq 
+echo "添加 skype 仓库..."
+sudo add-apt-repository -y "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
+echo "添加darktable 仓库..."
+sudo add-apt-repository -y ppa:pmjdebruijn/darktable-release
+sudo add-apt-repository -y ppa:pmjdebruijn/darktable-release-plus
+echo "添加防止黑屏的软件的ppa..."
+sudo add-apt-repository ppa:caffeine-developers/ppa -y
+echo "添加RabbitCvs 仓库..."
+sudo add-apt-repository ppa:rabbitvcs/ppa -y
+echo "添加Nodejs的仓库..."
+sudo apt-add-repository ppa:chris-lea/node.js -y
+echo "添加nitroshare仓库..."
+sudo add-apt-repository ppa:george-edison55/nitroshare -y
+echo "添加屏幕录像仓库..."
+sudo add-apt-repository ppa:maarten-baert/simplescreenrecorder -y
+echo "wireshark仓库..."
+sudo add-apt-repository ppa:dreibh/ppa -y
+echo "uGet仓库..."
+sudo sudo add-apt-repository ppa:plushuang-tw/uget-stable -y
+echo "macubuntu主题仓库..."
+sudo add-apt-repository ppa:noobslab/themes -y
+echo "Support i386 architecture"
+
+sudo dpkg --add-architecture i386
+
+echo "更新源...."
+sudo apt-get update
+
+echo "更新系统..."
+sudo apt-get upgrade -y
+
+echo "安装和配置 git svn等版本管理工具...."
+AptInstall "git gitk git-gui git-svn"
+
+echo "安装RabbitCVS的依赖..."
+AptInstall "python-nautilus python-configobj python-gtk2 python-glade2 python-svn python-dbus python-dulwich subversion meld"
+
+echo "安装RabbitCVS ..."
+AptInstall "rabbitvcs-cli  rabbitvcs-core rabbitvcs-gedit rabbitvcs-nautilus3" 
+
+echo "安装compiz特效管理..."
+AptInstall "compiz-plugins compiz-plugins-extra compizconfig-settings-manager "
+
+echo "安装 qt4..."
+AptInstall "libqt4-dev libqt4-dbg libqt4-gui libqt4-sql qt4-dev-tools qt4-doc qt4-designer qt4-qtconfig qtcreator"
+
+echo "安装java和java运行环境..."
+AptInstall "openjdk-7-jdk"
+
+echo "openssh..."
+AptInstall "openssh-server"
+
+echo "安装CodeBlock..."
+AptInstall "codeblocks g++ wx-common libwxgtk3.0-0 build-essential  wxformbuilder codeblocks-dbg codeblocks-contrib wx3.0-headers  wx3.0-i18n"
+
+echo "安装gimp,Inkscape等图形软件..."
+AptInstall "gimp Inkscape Dia darktable darktable-plugins-experimental darktable-plugins-legacy"
+
+echo "安装 fcitx 输入法..."
+AptInstall "fcitx fcitx-config-gtk fcitx-sunpinyin fcitx-googlepinyin fcitx-module-cloudpinyin"
+
+AptInstall "caffeine"
+
+AptInstall "openshot"
+
+AptInstall "keepass2"
+
+AptInstall "simplescreenrecorder"
+
+AptInstall "wireshark"
+
+AptInstall "aria2"
+
+AptInstall "unity-tweak-tool"
+
+echo "主题相关..."
+AptInstall "^mac-ithemes-v[0-9] ^mac-icons-v[0-9]"
+
+echo "安装搜狗输入法..."
+if [[ is_64 == 1 ]]; then
+	wget -c "http://pinyin.sogou.com/linux/download.php?f=linux&bit=64" sogoupinyin.deb
+else
+	wget -c "http://pinyin.sogou.com/linux/download.php?f=linux&bit=32" sogoupinyin.deb
+fi
+sudo dpkg -i sogoupinyin.deb&& rm sogoupinyin.deb
+
+echo "安装VirtualBox"
+AptInstall "virtualbox virtualbox-guest-additions-iso vde2"
+
+echo "安装 pidgin ... "
+AptInstall "libpurple0 pidgin pidgin-lwqq" 
+
+echo "安装goldendict..."
+AptInstall "goldendict goldendict-wordnet"
+
+echo "安装nautils相关..."
+AptInstall "nautilus-open-terminal nautilus-actions nautilus-dropbox"
+
+echo "安装其它杂七杂八.."
+AptInstall "unrar p7zip-full zhcon xbacklight shutter wallch wmctrl curl"
+AptInstall "vlc"
+AptInstall "lm-sensors"
+AptInstall "hddtemp"
+AptInstall "skype"
+AptInstall "grive-tools"
+AptInstall "dconf-editor dconf-cli"
+AptInstall "gparted"
+echo "exfat support ..."
+AptInstall "exfat-utils"
+
+echo "安装一些趣味命令行"
+AptInstall "sl cmatrix oneko libaa-bin toilet cowsay xcowsay xeyes moo"
+
+echo "Nodejs...."
+
+sudo apt-get install python-software-properties
+
+echo "嵌入式开发.."
+AptInstall "putty"
+AptInstall "openbsd-inetd tftp-hpa tftpd-hpa"
+AptInstall "nfs-kernel-server"
+AptInstall "minicom"
+AptInstall "bison flex mtd-utils"
+echo "设置tftp..."
+mkdir ~/Work/tftpboot
+chmod 777 ~/Work/tftpboot
+echo -e "RUN_DAEMON=\"yes\"" > tftpd-hpa
+echo -e "OPTIONS=\"-l -s -c /home/$(whoami)/Work/tftpboot\"" >> tftpd-hpa
+echo -e "TFTP_USERNAME=\"root\"" >> tftpd-hpa
+echo -e "TFTP_DIRECTORY=\"/home/$(whoami)/Work/tftpboot\"" >> tftpd-hpa
+echo -e "TFTP_ADDRESS=\"0.0.0.0:69\"" >> tftpd-hpa
+echo -e "TFTP_OPTIONS=\"--secure\"" >> tftpd-hpa
+sudo mv tftpd-hpa /etc/default/tftpd-hpa
+sudo service tftpd-hpa restart
+echo "设置nfs..."
+mkdir ~/Work/nfsroot
+chmod 777 ~/Work/nfsroot
+echo -e "/home/$(whoami)/Work/nfsroot *(rw,no_root_squash,no_all_squash,sync)" >  exports
+sudo mv exports /etc/
+sudo exportfs -av
+sudo service nfs-kernel-server restart 
+
+echo "安装zsh,tmux等并配置..."
+if [[ ! -d "linux-config" ]];then
+   git clone https://github.com/tracyone/dotfile
+fi
+cd dotfile;./install.sh;cd -
+
+echo "Install steam..."
+AptInstall "steam steam-launcher"
+
+echo "Install chromium and Pepper Flash Player"
+AptInstall "chromium-browser"
+AptInstall "pepperflashplugin-nonfree"
+sudo update-pepperflashplugin-nonfree --install
+
+echo "Install Sogou input ..."
+
+
+echo "安装字体...需要很长时间请耐心等待..."
+if [[ ! -d "program_font" ]];then
+   git clone https://github.com/tracyone/program_font
+fi
+if [[  $? -eq 0 ]]; then
+	sudo chmod -R a+x program_font/*
+	sudo mkdir -p /usr/share/fonts/MyFonts
+	mkdir ~/.fonts/
+	cp ./program_font/* ~/.fonts/
+	sudo cp ./program_font/* /usr/share/fonts/MyFonts
+	sudo fc-cache -f -v
+fi
+
+echo "为gnome-terminal安装solarized主题"
+git clone https://github.com/Anthony25/gnome-terminal-colors-solarized && cd gnome-terminal-colors-solarized && ./install.sh ; cd -
+
+if [[ -d "vim_conf" ]]; then
+	git clone https://github/tracyone/vim vim_conf
+fi
+if [[ $? -eq 0 ]]; then
+	cd vim_conf ;./build_vim.sh all
+	mkdir -p ~/.vim/autoload
+	curl -fLo ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim && gvim -c :PlugInstall &
+	cd -
+fi
+
+echo "避免ubuntu字体发虚..."
+sudo apt-get remove fonts-arphic-ukai ttf-wqy-zenhei fonts-arphic-uming -y
+
+echo "清除工作...."
+sudo apt-get autoremove -y
+sudo apt-get autoclean
+sudo apt-get clean
+echo "温馨提示：电脑将在30分钟后关机.."
+sudo shutdown -h 30
+
+# vim: set ft=sh fdm=marker foldlevel=0 foldmarker&: 
